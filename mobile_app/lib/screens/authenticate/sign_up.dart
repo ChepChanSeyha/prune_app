e4143b3743c6718314a8eef0e4904c43e275827a
kimdara <kim.d.dara@gmail.com>
2020-04-06 05:35:06 +0700

    test

---
+++ mobile_app/lib/screens/authenticate/sign_up.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:prune_app/services/auth.dart';
+import 'package:prune_app/shared/loading.dart';
+
+class SignUp extends StatefulWidget {
+  final Function toggleView;
+  SignUp({this.toggleView});
+
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
+
+  // Text field state
+  String email = '';
+  String password = '';
+  String telephone = '';
+  String error = '';
+
+  bool _obscureText = true;
+  bool loading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return loading ? Loading() : Scaffold(
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Container(
+            padding: EdgeInsets.symmetric(vertical: 70.0, horizontal: 30.0),
+            child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/images/icon.png',
+                      height: MediaQuery.of(context).size.width * 0.5,
+                      width: MediaQuery.of(context).size.width * 0.5,
+                    ),
+                    SizedBox(
+                      height: 50.0,
+                    ),
+                    TextFormField(
+                      validator: (val) => val.isEmpty ? 'Enter your email' : null,
+                      decoration: InputDecoration(
+                          hintText: 'Email', icon: Icon(Icons.email)),
+                      onChanged: (val) {
+                        setState(() => email = val);
+                      },
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      validator: (val) => val.length < 6
+                          ? 'Password must more than 6 characters'
+                          : null,
+                      decoration: InputDecoration(
+                          suffixIcon: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                _obscureText = !_obscureText;
+                              });
+                            },
+                            child: Icon(
+                              _obscureText ? Icons.visibility_off : Icons.visibility,
+                              semanticLabel:
+                              _obscureText ? 'show password' : 'hide password',
+                            ),
+                          ),
+                          hintText: 'Password',
+                          icon: Icon(Icons.vpn_key)),
+                      obscureText: _obscureText,
+                      onChanged: (val) {
+                        setState(() => password = val);
+                      },
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      validator: (val) => val.isEmpty ? 'Enter your phone number' : null,
+                      decoration: InputDecoration(
+                          hintText: 'Phone number', icon: Icon(Icons.phone)),
+                      onChanged: (val) {
+                        setState(() => telephone = val);
+                      },
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    RaisedButton(
+                      color: Color(0xffE91403),
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () async {
+                        if (_formKey.currentState.validate()) {
+                          setState(() => loading = true);
+                          dynamic result = await _auth.signUpWithEmailAndPassword(
+                              email, password);
+                          if (result == null) {
+                            setState(() {
+                              error = 'Something went wrong';
+                              loading = false;
+                            });
+                          }
+                        }
+                      },
+                    ),
+                    SizedBox(
+                      height: 50.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'Already have an account? ',
+                          style: TextStyle(color: Colors.grey[800]),
+                        ),
+                        GestureDetector(
+                          child: Row(
+                            children: <Widget>[
+                              Text('Sign In',
+                                  style: TextStyle(
+                                    color: Color(0xffE91403),
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    decoration: TextDecoration.underline,
+                                  ))
+                            ],
+                          ),
+                          onTap: () {
+                            widget.toggleView();
+                          },
+                        )
+                      ],
+                    ),
+                  ],
+                ))),
+      ),
+    );
+  }
+}
---
+++ mobile_app/lib/screens/home/profile.dart
@@ -0,0 +1,142 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:prune_app/shared/loading.dart';
+
+import 'charts.dart';
+
+class Profile extends StatefulWidget {
+  @override
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  bool loading = false;
+
+  @override
+  Widget build(BuildContext context) {
+
+    var dpc = DatumLegendWithMeasures.withSampleData();
+
+    return loading ? Loading() : Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('My Page'),
+        actions: <Widget>[
+//          Icon(Icons.search),
+            FlatButton.icon(
+              onPressed: () async {
+                await _auth.signOut();
+              },
+              icon: Icon(
+                Icons.person,
+                color: Colors.white,
+              ),
+              label: Text(
+                'Logout',
+                style: TextStyle(color: Colors.white),
+              ))
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Color(0xff5BBDF4),
+              ),
+              child: Text(
+                'Drawer Header',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('Messages'),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width * 1,
+              height: MediaQuery.of(context).size.width/2.5,
+              child: Image.asset('assets/images/image.png', fit: BoxFit.fitHeight, alignment: Alignment.center),
+            ),
+            Container(
+              color: Colors.blue[100],
+              width: MediaQuery.of(context).size.width * 1,
+              height: MediaQuery.of(context).size.width/3,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Makara Dae', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),),
+                      Text('Phnom Penh, Cambodia'),
+                    ],
+                  ),
+                  ButtonTheme(
+                    height: 50.0,
+                    minWidth: 100.0,
+                    child: RaisedButton(
+                      child: Text('EDIT'),
+                      onPressed: () {},
+                      textColor: Colors.white,
+                      color: Color(0xff5BBDF4),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10, bottom: 10),
+              child: Container(
+                width: MediaQuery.of(context).size.width * 1,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 20),
+                        child: Text('Learning history', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),),
+                      ),
+                      Center(
+                        child: Container(
+                          //color: Colors.blue,
+                          height: 350.0,
+                          width: 350.0,
+                          child: dpc,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
