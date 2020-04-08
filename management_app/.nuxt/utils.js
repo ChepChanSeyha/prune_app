4f625936978711b0bece498680e61ed92e8dd483
CalvinSey <40975050+ChepChanSeyha@users.noreply.github.com>
2020-04-08 23:53:21 +0700

    Revert "Merge branch 'master' into seyha"
    This reverts commit 85474592543c68abfbf5d71039ef0468abced7ff, reversing
    changes made to 72ff60ade0b9a6e0993bc86c72b4f0b871662aea.

--- management_app/.nuxt/utils.js
+++ management_app/.nuxt/utils.js
@@ -150,7 +150,7 @@ export async function setContext (app, context) {
       payload: context.payload,
       error: context.error,
       base: '/',
-      env: {"BASE_URL":"http://localhost:3000"}
+      env: {}
     }
     // Only set once
     if (context.req) {
