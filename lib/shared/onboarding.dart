594a50596747674013421712b2965638e2fb8087
CalvinSey <40975050+ChepChanSeyha@users.noreply.github.com>
2020-04-13 09:17:09 +0700

    delete dara project

---
+++ lib/shared/onboarding.dart
@@ -0,0 +1,140 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:prune_app/screens/authenticate/sign_in.dart';
+
+class OnBoardingPage extends StatefulWidget {
+  OnBoardingPage({Key key}) : super(key: key);
+
+  _OnBoardingPageState createState() => _OnBoardingPageState();
+}
+
+class _OnBoardingPageState extends State<OnBoardingPage> {
+  final int _totalPages = 4;
+  final PageController _pageController = PageController(initialPage: 0);
+  int _currentPage = 0;
+
+  Widget _buildPageIndicator(bool isCurrentPage) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 350),
+      margin: EdgeInsets.symmetric(horizontal: 2.0),
+      height: isCurrentPage ? 11.0 : 7.0,
+      width: isCurrentPage ? 11.0 : 7.0,
+      decoration: BoxDecoration(
+        color: isCurrentPage ? Colors.red  : Colors.red[300],
+        borderRadius: BorderRadius.all(Radius.circular(12)),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        child: Container(
+          child: PageView(
+            controller: _pageController,
+            onPageChanged: (int page) {
+              _currentPage = page;
+              setState(() {});
+            },
+            children: <Widget>[
+              _buildPageContent(
+                  image: 'assets/images/icon.png',
+                  body:
+                  'Lorem dolor sit amet consecteur adipiscing elit sed do eiusmod tempor incididunt ut ero labor et dolor'),
+              _buildPageContent(
+                  image: 'assets/images/icon.png',
+                  body:
+                  'Lorem dolor sit amet consecteur adipiscing elit sed do eiusmod tempor incididunt ut ero labor et dolor'),
+              _buildPageContent(
+                  image: 'assets/images/icon.png',
+                  body:
+                  'Donec facilisis tortor ut augue lacinia, at viverra est semper. Sed sapien metus, scelerisque nec pharetra id, tempor a tortor. Pellentesque non dignissim neque.'),
+              _buildPageContent(
+                  image: 'assets/images/icon.png',
+                  body:
+                  'Donec facilisis tortor ut augue lacinia, at viverra est semper. Sed sapien metus, scelerisque nec pharetra id, tempor a tortor. Pellentesque non dignissim neque.')
+            ],
+          ),
+        ),
+      ),
+      bottomSheet: _currentPage != 3
+          ? Container(
+        height: MediaQuery.of(context).size.height/12,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            FlatButton(
+              onPressed: () {
+                _pageController.animateToPage(_totalPages, duration: Duration(milliseconds: 400), curve: Curves.linear);
+                setState(() {});
+              },
+              splashColor: Colors.red[50],
+              child: Text(
+                'SKIP',
+                style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              child: Row(children: [
+                for (int i = 0; i < _totalPages; i++) i == _currentPage ? _buildPageIndicator(true) : _buildPageIndicator(false)
+              ]),
+            ),
+            FlatButton(
+              onPressed: () {
+                _pageController.animateToPage(_currentPage + 1, duration: Duration(milliseconds: 400), curve: Curves.linear);
+                setState(() {});
+              },
+              child: Text(
+                'NEXT',
+                style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+              ),
+            )
+          ],
+        ),
+      )
+          : GestureDetector(
+        onTap: () => {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => SignIn()))
+        },
+        child: Container(
+          height: Platform.isIOS ? 70 : 60,
+          color: Colors.blue,
+          alignment: Alignment.center,
+          child: Text(
+            'GET STARTED NOW',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPageContent({
+    String image,
+    String title,
+    String body,
+  }) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Center(
+            child: Image.asset(image, width: MediaQuery.of(context).size.height/3,),
+          ),
+          SizedBox(height: 40),
+          Text(
+            body,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 14, height: 1.5),
+          ),
+        ],
+      ),
+    );
+  }
+}
