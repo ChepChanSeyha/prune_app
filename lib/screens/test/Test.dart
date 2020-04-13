7528d7c6079fe00b459ab5eb4963a7610810f515
CHEA-Menghong <cheamenghong123@gmail.com>
2020-04-13 14:34:00 +0700

    update test screen

--- lib/screens/test/Test.dart
+++ lib/screens/test/Test.dart
@@ -20,35 +20,57 @@ class _TestState extends State<Test> {
             height: 68,
             color: Colors.red,
             child: Center(
-              child: Text('Test1',style:TextStyle(color: Colors.white) ,),
+              child: Text('Test1',style:TextStyle(color: Colors.white,fontSize: 24) ,),
             ),
           ),
           SizedBox(height: 10,),
-          Container(
-            height: 68,
-            color: Colors.white,
-            child: Center(
-              child: Text('A',style:TextStyle(color: Colors.red)),
-            ),
+          GestureDetector(
+              onTap: (){
+                final snackBar = SnackBar(content: Text("Tap A"),backgroundColor: Colors.red[300],);
+                Scaffold.of(context).showSnackBar(snackBar);
+                print(" A clicked");},
+              child: new Container(
+                width: 500.0,
+                height: 68,
+                color: Colors.white,
+                child: Center(
+                  child: Text('A',style:TextStyle(color: Colors.red))
+                ),
+              )
           ),
           SizedBox(height: 10,),
-          Container(
-            height: 68,
-            color: Colors.white,
-            child: Center(
-              child: Text('B',style:TextStyle(color: Colors.red)),
-            ),
+          GestureDetector(
+              onTap: (){
+                final snackBar = SnackBar(content: Text("Tap B"),backgroundColor: Colors.red[300],);
+                Scaffold.of(context).showSnackBar(snackBar);
+                print("B clicked");},
+              child:  Container(
+                width: 500.0,
+                height: 68,
+                color: Colors.white,
+                child:  Center(
+                  child: Text('B',style:TextStyle(color: Colors.red)),
+                ),
+              )
           ),
           SizedBox(height: 10,),
-          Container(
-            height: 68,
-            color: Colors.white,
-            child: Center(
-              child: Text('C',style:TextStyle(color: Colors.red)),
-            ),
+          GestureDetector(
+              onTap: (){
+                final snackBar = SnackBar(content: Text("Tap C"),backgroundColor: Colors.red[300],);
+                Scaffold.of(context).showSnackBar(snackBar);
+                print("C clicked");},
+              child:  Container(
+                width: 500.0,
+                height: 68,
+                color: Colors.white,
+                child:  Center(
+                  child: Text('C',style:TextStyle(color: Colors.red)),
+                ),
+              )
           ),
+      // The GestureDetector wraps the button.
 
-        ],
+      ],
       ),
 
     );
